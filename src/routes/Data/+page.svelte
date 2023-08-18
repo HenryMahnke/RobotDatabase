0ff109e1998cd23caecf755b10291b21944ddc71
HenryMahnke <henry.mahnke16@gmail.com>
2023-08-17 21:58:53 -0500

    init

---
+++ src/routes/Data/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Search, Button } from 'flowbite-svelte';
+	import { Icon } from 'flowbite-svelte-icons';
+	function handleVoiceBtn() {
+		alert('You clicked voice button');
+	}
+</script>
+
+<form class="flex gap-2">
+	<Search
+		size="md"
+		class="flex gap-2 items-center"
+		placeholder="Search Mockups, Logos, Design Templates..."
+	>
+		<button type="button" on:click={handleVoiceBtn} class="outline-none">
+			<Icon name="microphone-solid" class="w-4 h-4" />
+		</button>
+	</Search>
+	<Button size="sm">
+		<Icon name="search-outline" class="w-5 h-5 mr-2 -ml-1" />
+		Search
+	</Button>
+</form>
+
+<h1>enter your data brenda</h1>
